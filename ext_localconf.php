5173bcf11bd9d5b50792530a278279475ed34360
Eric Harrer <info@eric-harrer.de>
2023-06-30 22:27:56 +0200

    Add extension setting to auto include TypoScript

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,23 @@
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die();
 
 (static function () {
+    $autoIncludeTypoScript = GeneralUtility::makeInstance(ExtensionConfiguration::class)
+        ->get('ticker_messages', 'autoIncludeTypoScript');
+
+    if($autoIncludeTypoScript) {
+        ExtensionManagementUtility::addTypoScript(
+            'ticker_messages',
+            'setup',
+            "@import 'EXT:ticker_messages/Configuration/TypoScript/setup.typoscript'"
+        );
+    }
+
     /**
      * Adding the default Page TSconfig
      */
