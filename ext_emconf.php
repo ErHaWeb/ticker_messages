fee9515bf3a6193d1a3a961159ac7b340300ca7e
Eric Harrer <info@eric-harrer.de>
2023-07-11 11:29:08 +0200

    [BUGFIX] Fix ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,20 @@
 <?php
-declare(strict_types=1);
-
-defined('TYPO3') or die();
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Ticker Messages',
     'description' => 'This is a demo extension that demonstrates how to output custom records without a "Classes" directory in the extension with DatabaseQueryProcessor in a categorized list view. Thanks to Thomas Löffler and his extension "just_news" for the inspiration for this approach.',
-    'category' => 'fe',
-    'version' => '1.0.0',
-    'state' => 'stable',
+    'category' => 'plugin',
     'author' => 'Eric Harrer',
     'author_email' => 'info@eric-harrer.de',
+    'author_company' => 'eric-harrer.de',
+    'state' => 'stable',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-12.4.99',
-            'fluid_styled_content' => ''
         ],
-        'conflicts' => [],
-        'suggests' => []
-    ]
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
 ];
